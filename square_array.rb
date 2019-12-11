61ee5c81ccdfe73cf363934e6058f4b12d86cfae
Autonyo Sanders <tonio.sanders@gmail.com>
2019-12-11 20:39:01 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,8 @@
 def square_array(array)
   squared_array = []
-  array.each do |num|
-    square = num ** 2
-    squared_array << square
+  array.each do |number|
+    squared = number ** 2
+    squared_array << squared
   end
   squared_array
 end
